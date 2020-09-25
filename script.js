ef5e116242ee6842f6ecbe2079d49d56414087a9
yassinerh28 <yassineelrhazouan28@gmail.com>
2020-09-25 19:17:48 +0100

    arrows

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var pagesContainer = document.getElementById("pagesContainer");
 var pages = document.getElementsByClassName("page");
 var images = document.getElementsByClassName("image");
+var arrowRight = document.getElementsByClassName("right")[0];
+var arrowLeft = document.getElementsByClassName("left")[0];
+var index = 0;
 
 for (i = 0; i < images.length; i++) {
 	pagesContainer.innerHTML += '<div class="page"></div>';
@@ -13,21 +16,26 @@ for (i = 0; i < pages.length; i++) {
 		image.classList.add("selected");
 		page.classList.add("selected");
 	}
-	page.addEventListener("click", change);
+	page.addEventListener("click", function change() {select(this)});
 }
 
-function change() {
+arrowRight.addEventListener("click", function change() {select(pages[(index+1)%pages.length])});
+arrowLeft.addEventListener("click", function change() {select(pages[(index+pages.length-1)%pages.length])});
+
+
+function select(p) {
 	for (i = 0; i < pages.length; i++) {
 		var page = pages[i];
 		var image = images[i];
 		page.classList.remove("selected");
 		image.classList.remove("selected");
 	}
-	this.classList.add("selected");
+	p.classList.add("selected");
 	for (i = 0; i < pages.length; i++) {
 		page = pages[i];
-		if (page === this) {
+		if (page === p) {
 			images[i].classList.add("selected");
+			index = i;
 			break;
 		}
 	}
